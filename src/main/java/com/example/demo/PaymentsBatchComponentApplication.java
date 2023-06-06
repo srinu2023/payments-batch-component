76c4884eb8192f1df6ba87179e65e4a8dc5b2b26
SRINU149 <st2020java@gmail.com>
2023-06-06 12:36:23 +0530

    batch files added

---
+++ src/main/java/com/example/demo/PaymentsBatchComponentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsBatchComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsBatchComponentApplication.class, args);
+	}
+
+}
